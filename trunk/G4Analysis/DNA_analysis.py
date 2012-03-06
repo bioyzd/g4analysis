388df1c3b482d140d196fb51bd1232c425447e1f
zhuhongaaa@gmail.com <zhuhongaaa@gmail.com@536e5464-63c4-44cf-b88c-d5d000304d32>
2012-03-06 12:48:10 +0000

    change

--- trunk/G4Analysis/DNA_analysis.py
+++ trunk/G4Analysis/DNA_analysis.py
@@ -1,7 +1,7 @@
 #1/usr/bin/env python
 #-*- coding:utf-8 -*-
 '''
-Created at 2010.01.14
+Created at 2012.03.06
 @version: 0.0.3
 @change:\n
     - 2010.01.14.
@@ -35,6 +35,7 @@ import sys
 import os
 import usage
 import MDAnalysis
+import DNA_param
 from math import cos, sin, sqrt
 import math
 import numpy
@@ -43,8 +44,7 @@ from numpy import dot
 
 import DNA_matrix
 
-
-def Get_parallel_result(traj_file, coor_file, base_list_1, base_list_2, output_name,skip=1, dt=1,begin=0,end=-1):
+def Get_DNA_result(traj_file, coor_file, base_list_1, base_list_2, output_name,skip=1, dt=1,begin=0,end=-1):
     '''
     Reading the traj file and the coordinate file like *.pdb or *.gro. With the base serial choosed,  get the
     rotation matrix for this base. and write it's to a output file with some syntax.
@@ -185,12 +185,10 @@ def Get_parallel_result(traj_file, coor_file, base_list_1, base_list_2, output_n
     print "The parallel analysis finished"
     print "The result are in file: %s" %output_name
 
-def Get_parallel_fromTOP( coor_file, base_list_1, base_list_2):
+def Get_para_fromTOP( coor_file, base_list_1, base_list_2,CALCU="step"):
     '''
-    Get the parallel parameters from coor_file.
+    get pair parameters from coor_file
     '''
-#    if coor_file.endswith('.top'):
-#        Atom_list=amber_top
 
     Atom_list=Simple_atom.Get_Simple_atom_list(coor_file)
     residue_list=Simple_atom.Get_Residue_list(Atom_list)
@@ -220,10 +218,6 @@ def Get_parallel_fromTOP( coor_file, base_list_1, base_list_2):
     base_atom_list_1=[DNA_matrix.Get_baseID_list(Atom_list,j) for j in base_list_1]
     base_atom_list_2=[DNA_matrix.Get_baseID_list(Atom_list,j) for j in base_list_2]
 
-#    print base_atom_list_1
-#    print base_atom_list_2
-
-
     r1=[]
     '''the group 1 rotate list'''
     r2=[]
@@ -237,160 +231,21 @@ def Get_parallel_fromTOP( coor_file, base_list_1, base_list_2):
                 for x in base_atom_list_1[m] ]
 #        print temp_list
         result = DNA_matrix.Get_rotate_matrix(numpy.array(temp_list), base_name_list_1[m][0])
-        c1.append(numpy.array(temp_list))
-        r1.append(result)
+        r1.append(result[0])
+        c1.append(result[1])
 
     for m in range(len(base_name_list_2)):
         temp_list = [ [atom_list[x].atom_coor_x*10, atom_list[x].atom_coor_y*10,atom_list[x].atom_coor_z*10] \
                 for x in base_atom_list_2[m] ]
 #        print temp_list
         result = DNA_matrix.Get_rotate_matrix(numpy.array(temp_list), base_name_list_2[m][0])
-        c2.append(numpy.array(temp_list))
-        r2.append(result)
+        r2.append(result[0])
+        c2.append(result[1])
 
-    orient_group_1,origin_group_1 = DNA_matrix.Get_group_rotmat(r1,len(base_name_list_1))
-    orient_group_2,origin_group_2 = DNA_matrix.Get_group_rotmat(r2,len(base_name_list_2))
-    RMSD1=DNA_matrix.Get_group_RMSD(base_name_list_1,c1,origin_group_1,orient_group_1)
-    RMSD2=DNA_matrix.Get_group_RMSD(base_name_list_2,c2,origin_group_2,orient_group_2)
-
-    if numpy.dot(orient_group_1, orient_group_2)<0:
-        orient_group_2 = orient_group_2*(-1)
-
-    orient_total = DNA_matrix.Rotate_2_vector(orient_group_1, orient_group_2)
-    dist_vector = numpy.array(origin_group_2)-numpy.array(origin_group_1)
-
-    dist = abs(dot(orient_total, dist_vector))
-    gamma = dot(orient_group_1, orient_group_2) 
-    gamma = math.acos(gamma)*180/3.1416
-
-    print  "rise:  %6.3f\tangle:   %6.3f\tRMSD_1:    %6.4f\tRMSD_2:   %6.4f\n" %(dist, gamma,RMSD1,RMSD2)
-
-
-def Get_RMSD_result(traj_file, coor_file, base_list, output_name,skip=1, dt=1,begin=0,end=-1):
-    '''
-    Reading the traj file and the coordinate file like *.pdb or *.gro. With the base serial choosed,  get the
-    rotation matrix for this base. and write it's to a output file with some syntax.
-    '''
-    print "  init......"
-    START_TIME=Time.time()
-    LIST_NUM=len(base_list)
-
-    
-    Atom_list=Simple_atom.Get_Simple_atom_list(coor_file)
-    residue_list=Simple_atom.Get_Residue_list(Atom_list)
-    
-    base_name_list=list()
-    base_atom_list=list()
-
-    for i in range(LIST_NUM):
-        if os.path.isfile(output_name[i]):
-            print "backup %s to %s" %(output_name[i],"#"+output_name[i]+"#")
-            try:
-                os.rename(output_name[i],"#"+output_name[i]+"#")
-            except OSError,e: 
-                print e
-                print "the file %s will be overwrited!" %output_name[i]
-
-        fp = open(output_name[i], 'w')
-        fp.write("#Group list: ")
-        for j in base_list[i]:
-            fp.write("%d\t " %j)
-        fp.write("\n")
-        fp.write("#skip:%d\n" %skip)
-        fp.write("#time(ns)   RMSD(A)\n")
-
-#        base_name_list.append( [residue_list[j-1] for j in base_list[i]])
-        temp_list=list()
-        for m in base_list[i]:
-            for n in residue_list:
-                if n[1]==m:
-                    temp_list.append(n)
-                    break
-                else:
-                    pass
-        base_name_list.append(temp_list)
-#        print base_name_list
-
-        base_atom_list.append( [DNA_matrix.Get_baseID_list(Atom_list,j) for j in base_list[i]])
-
-    u=MDAnalysis.Universe(coor_file,traj_file)
-
-
-
-    if traj_file.endswith("mdcrd") or traj_file.endswith("dcd"):
-        pass
+    if CALCU=="pair":
+        return DNA_param.base_pair_parameters(r1[0],r2[0],c1[0],c2[0])
     else:
-        dt=u.trajectory.dt
-
-
-    for ts in u.trajectory:
-        time=(ts.frame-1)*dt
-   #     print begin,end
-        if time < float(begin):
-   #         print time
-            continue
-        if end > 0 and time > float(end):
-            break
-
-        if ts.frame % skip == 0 :
-            for i in range(LIST_NUM):
-                r1=[]
-                '''the group 1 rotate list'''
-                c1=[]
-                '''the group 1 coordinate list'''
-                for m in range(len(base_name_list[i])):
-                    temp_list = [ [ts._x[x-1], ts._y[x-1], ts._z[x-1]] for x in base_atom_list[i][m] ]
-                    result = DNA_matrix.Get_rotate_matrix(numpy.array(temp_list), base_name_list[i][m][0])
-                    c1.append(numpy.array(temp_list))
-                    r1.append(result)
-
-                orient_group,origin_group = DNA_matrix.Get_group_rotmat(r1,len(base_name_list[i]))
-                RMSD=DNA_matrix.Get_group_RMSD(base_name_list[i],c1,origin_group,orient_group)
-
-                time=(ts.frame-1)*dt
-
-                if ts.frame % 10 ==0:
-                    NOW_TIME=Time.time()
-                    usage.echo("  analysis frame %6d, time %8.1f ps, time used %8.2f s\r" %(ts.frame, time,NOW_TIME-START_TIME))
-                fp=open(output_name[i], 'a')
-                fp.write( " %7.4f\t  %6.4f\n" %(time/1000,RMSD))
-                fp.close()
-
-    print "Finished calculating the RMSD."
-    print "The result are in file: %s" %output_name
-
-
-def Get_RMSD_fromTOP(coor_file, base_list):
-    '''
-    Calculate the RMSD from coor_file.
-    '''
-    LIST_NUM=len(base_list)
-
-    atom_list=Simple_atom.Get_Simple_atom_list(traj_file)
-    residue_list=Simple_atom.Get_Segment_list(atom_list)
-
-    base_name_list=list()
-    base_atom_list=list()
-    for i in range(LIST_NUM):
-        base_name_list.append( [residue_list[j-1] for j in base_list[i]])
-        base_atom_list.append( [DNA_matrix.Get_baseID_list(atom_list,j) for j in base_list[i]])
-
-    for i in range(LIST_NUM):
-        r1=[]
-        '''the group 1 rotate list'''
-        c1=[]
-        '''the group 1 coordinate list'''
-        for m in range(len(base_name_list[i])):
-            temp_list = [ [atom_list[x-1].atom_coor_x, atom_list[x-1].atom_coor_y, atom_list[x-1].atom_coor_z] \
-                    for x in base_atom_list[i][m] ]
-            result = DNA_matrix.Get_rotate_matrix(numpy.array(temp_list), base_name_list[i][m][0])
-            c1.append(numpy.array(temp_list))
-            r1.append(result)
-
-        orient_group,origin_group = DNA_matrix.Get_group_rotmat(r1,len(base_name_list[i]))
-        RMSD=DNA_matrix.Get_group_RMSD(base_name_list[i],c1,origin_group,orient_group)
-
-        print "RMSD=%f" %RMSD
-    return 
-
+        middle_r1,middle_c1=DNA_param.middle_frame(r1[0],r1[1],c1[0],c1[1])
+        middle_r2,middle_c2=DNA_param.middle_frame(r2[0],r2[1],c2[0],c2[1])
+        return  DNA_param.base_step_parameters(middle_r1,middle_r2,middle_c1,middle_c2)
 
